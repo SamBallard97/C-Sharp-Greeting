1af1d28c7e197d9b9c0598a94f4cd9a5e0b22321
Sam <sam.ballard1997@googlemail.com>
2021-02-19 16:34:32 +0000

    Added 2 unit tests

---
+++ GreetingApp/Controllers/GreetingControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GreetingApp.Controllers
+{
+    public class GreetingControllerTests
+    {
+        public Mock<IItem> _mockIItem;
+
+        public GreetingControllerTests()
+        {
+            _mockIItem = new Mock<IItem>();
+        }
+    }
+
+    public class GreetingControllerGetTest : GreetingControllerTests
+    {
+        [Fact]
+        public void GreetingControllerGetReturnsTest()
+        {
+            //arrange
+            var greetingController = new GreetingController(_mockIItem.Object);
+            var datedWelcomeMessage = DateTime.Now + "welcome";
+
+            //act
+            var result = greetingController.GetWelcomeMessage();
+
+            //assert
+            Assert.IsType<string>(result);
+            Assert.Equal(datedWelcomeMessage, result);
+        }
+
+        [Fact]
+        public void GreetingControllerGetItemsReturnsTest()
+        {
+            //arrange
+            var greetingController = new GreetingController(_mockIItem.Object);
+            var exampleItem = new Item() { Age = 4, Name = "Item1" };
+            //act
+            var result = greetingController.GetItems();
+
+            //assert
+            Assert.IsType<List<Item>>(result);
+            Assert.Equal(exampleItem, result[0]);
+        }
+    }
+}

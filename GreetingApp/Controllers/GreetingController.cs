7515cef90fa5ca33f99aafa58d21f381b10bdadb
Melissa Astbury <melissaastbury@hotmail.com>
2021-02-19 14:46:12 +0000

    first commit

---
+++ GreetingApp/Controllers/GreetingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreetingApp.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class GreetingController
+    {
+        private IItem _item;
+
+        public GreetingController(IItem item)
+        {
+            _item = item;
+        }
+
+        [HttpGet("greeting")]
+        public string GetWelcomeMessage()
+        {
+            var timeStamp = DateTime.Now;
+            return timeStamp + "welcome";
+        }
+
+        [HttpGet("all")]
+        public List<Item> GetItems()
+        {
+            return _item.GetAllItems();
+        }
+    }
+}
---
+++ GreetingApp/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace GreetingApp
+{
+    public class Item : IItem
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<Item> GetAllItems()
+        {
+            var itemList = new List<Item>()
+            {
+                new Item
+                {
+                    Name = "Item1",
+                    Age = 4,
+                }
+            };
+            return itemList;
+        }
+    }
+}
